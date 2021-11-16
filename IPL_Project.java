6d821431d38b308d2e5a4e15b726133d94c6c302
poorwakesharwani <48479442+poorwakesharwani@users.noreply.github.com>
2021-11-16 17:48:29 +0530

    Add files via upload
    completed 2nd question

--- IPL_Project.java
+++ IPL_Project.java
@@ -4,8 +4,7 @@ import java.io.FileReader;
 import java.util.HashMap;
 
 public class IPL_Project {
-
-    public static void main(String args[]){
+    void Question1(){
         HashMap<String,Integer>numberof_match=new HashMap<>();
         try {
             FileReader filereader = new FileReader("C:/Users/pkesharwani/Desktop/IPL_Project/matches.csv");
@@ -13,9 +12,9 @@ public class IPL_Project {
             String[] nextRecord;
             csvReader.readNext();
             while ((nextRecord = csvReader.readNext()) != null) {
-                   //System.out.println(nextRecord[1]);
+                //System.out.println(nextRecord[1]);
                 if(numberof_match.containsKey(nextRecord[1])){
-                    System.out.println("yes"+numberof_match.get(nextRecord[1]));
+                  //  System.out.println("yes"+numberof_match.get(nextRecord[1]));
                     numberof_match.put(nextRecord[1],numberof_match.get(nextRecord[1])+1);
                 }else {
                     numberof_match.put(nextRecord[1], 1);
@@ -26,5 +25,28 @@ public class IPL_Project {
             e.printStackTrace();
         }
         System.out.println(numberof_match);
+    }
+    void Question2(){
+        int totalnormalmatch=0;
+        try {
+            FileReader filereader = new FileReader("C:/Users/pkesharwani/Desktop/IPL_Project/matches.csv");
+            CSVReader csvReader = new CSVReader(filereader);
+            String[] nextRecord;
+            csvReader.readNext();
+            while ((nextRecord = csvReader.readNext()) != null) {
+                //System.out.println(nextRecord[8]);
+                if(nextRecord[8].equals("normal")){
+                    totalnormalmatch++;
+                }
+
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(totalnormalmatch);
+    }
+
+    public static void main(String args[]) {
     }
 }
