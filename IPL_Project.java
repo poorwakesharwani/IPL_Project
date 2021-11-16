6a39599b57bc37c367805667f57e0889fa6c8a70
poorwakesharwani <48479442+poorwakesharwani@users.noreply.github.com>
2021-11-16 14:16:46 +0530

    Add files via upload
    Number of match count in one season

--- IPL_Project.java
+++ IPL_Project.java
@@ -1,20 +1,30 @@
 import com.opencsv.CSVReader;
 
 import java.io.FileReader;
+import java.util.HashMap;
 
 public class IPL_Project {
 
     public static void main(String args[]){
+        HashMap<String,Integer>numberof_match=new HashMap<>();
         try {
             FileReader filereader = new FileReader("C:/Users/pkesharwani/Desktop/IPL_Project/matches.csv");
             CSVReader csvReader = new CSVReader(filereader);
             String[] nextRecord;
+            csvReader.readNext();
             while ((nextRecord = csvReader.readNext()) != null) {
-                   System.out.println(nextRecord[1]);
+                   //System.out.println(nextRecord[1]);
+                if(numberof_match.containsKey(nextRecord[1])){
+                    System.out.println("yes"+numberof_match.get(nextRecord[1]));
+                    numberof_match.put(nextRecord[1],numberof_match.get(nextRecord[1])+1);
+                }else {
+                    numberof_match.put(nextRecord[1], 1);
+                }
             }
         }
         catch (Exception e) {
             e.printStackTrace();
         }
+        System.out.println(numberof_match);
     }
 }
